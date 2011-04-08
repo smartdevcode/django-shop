f4da687351bbf32d6faaa2d186565b52603158ab
SeniorDevCode <senior.dev.software@gmail.com>
2011-04-08 08:28:50 +0200

    Reverted removal of cart/update/ (with action=put), as suggested.

--- shop/urls.py
+++ shop/urls.py
@@ -17,12 +17,11 @@ urlpatterns = patterns('',
     # Cart
     url(r'^cart/delete/$', CartDetails.as_view(action='delete'), # DELETE 
         name='cart_delete'),
+    url(r'^cart/update/$', CartDetails.as_view(action='put'), # DELETE 
+        name='cart_update'),
     url('^cart/item/$', CartDetails.as_view(action='post'), # POST
         name='cart_item_add' ),
     url(r'^cart/$', CartDetails.as_view(), name='cart'), # GET
-    url(r'^cart/update/$', CartDetails.as_view(action='put'), 
-        name='cart_update'),
-
     # CartItems
     url('^cart/item/(?P<id>[0-9A-Za-z-_.//]+)$', CartItemDetail.as_view(action='put'),
         name='cart_item_update' ),
