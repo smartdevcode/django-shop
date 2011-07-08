feb56c8b323edf599eec936fb9df6429ed87d9cc
SeniorDevCode <senior.dev.software@gmail.com>
2011-07-08 13:03:35 +0200

    Fixed custom cartitem model loading

--- shop/models/ordermodel.py
+++ shop/models/ordermodel.py
@@ -65,7 +65,6 @@ class OrderManager(models.Manager):
             i.order = o
             i.product_reference = item.product.id
             i.product_name = item.product.name
-            i.product = item.product
             i.unit_price = item.product.get_price()
             i.quantity = item.quantity
             i.line_total = item.line_total
